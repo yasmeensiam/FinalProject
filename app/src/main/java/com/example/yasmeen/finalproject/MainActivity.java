65248269449787d0334829223c5153c1e798754c
yasmeen <yasmeenseyam@gmail.com>
2016-03-08 00:24:20 +0200

    Initial commit

---
+++ app/src/main/java/com/example/yasmeen/finalproject/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.yasmeen.finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity  {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
+
